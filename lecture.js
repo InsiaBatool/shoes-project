182e9fa050a3948a9daf719e45a79bc131ec95d9
unknown <insiabatool1512@gmail.com>
2024-04-22 13:37:12 +0500

    first commit

---
+++ lecture.js
@@ -0,0 +1,45 @@
+//looping structure
+//for
+
+// for(let i=1; i<=5; i++)
+// {
+//     console.log(i*2);
+// }
+
+// for(let i=5; i>=1;i--)
+// {
+//     console.log(i);
+// }
+
+
+// for(let i=1;i<=19;i=i+1)
+// {
+//     console.logi(i);
+// }
+
+
+// for(let i=1; i<=20;i++){
+// if(i%2==1){
+//     console.log(i);
+// }
+// }
+
+
+
+let i=1;
+while(i<=20){
+    if (i%2==0){
+        console.log(i);
+    }
+    i++;
+    
+}
+
+// console.log("javascript")
+
+
+
+
+
+
+
---
+++ second.js
@@ -0,0 +1,190 @@
+//  for (let count = 0; count <=5; count++) {
+//     console.log("waseela");
+//     };
+//       console.log("loop has ended");
+
+
+// sum = 0;
+// for (let i = 0; i <= 15; i++) {
+//     sum = sum+i
+// };
+// console.log("sum=",sum);
+
+
+
+// let i=1;
+// while (i<=5) {
+//     console.log("welcome to nagar");
+//     i++
+// };
+
+
+// let i=20;
+// do {
+//     console.log("nice dream");
+//     i++;
+    
+// } while (i<=10);
+
+
+
+//for of loop
+// let str="hellow";
+// let length=0
+// for (let val of str) {
+//     console.log("val=",val);
+//     length++
+// }
+// console.log("length=",length);
+
+
+
+//for in loop
+// let student={
+//     name : "kashana",
+//     age : 16,
+//     class : 9,
+
+// };
+// for (let key in student) {
+    
+//         console.log("key=",key,"value=",student[key]);
+        
+//      }
+
+// let i=1;
+// while (1>=2) {
+//     console.log("i=",1);
+//     i++
+    
+// }
+
+// sum=0;
+// let n=100;
+// for(let i=1 ; i<=n ; i++ ){
+//     sum=sum+i
+// }
+// console.log("sum=" , sum);
+
+
+// for(let i=1 ; i<=5; i++){
+//     console.log("i=",i);
+// }
+
+
+// let i=1;
+// while (i<=5) {
+//     console.log("apna colleg");
+//     i++;
+    
+// }
+
+
+// let i=20;
+// do{
+//     console.log("wellcom to nagar");
+//     i++;
+// } while(i<=10);
+
+// let i=10;
+// do{
+//     console.log("wellecome to nagar");
+//     i++;
+// }while (i<=5) 
+
+
+//for of loop (ye strings or arrays k leiy use hta h)
+// let str="teacher";
+// size=0;
+// for(let val of str){
+//     console.log("val=",val);
+//     size++;
+// }
+// console.log("string size=",size);
+
+
+
+
+
+//for in loop (ye object ki keys k leiy use hota h or ye b arrys k b use hota h)
+//let student={
+   // name : "Waseela",
+    //age : 24,
+    //cgpa : 3.4,
+    //depatrment : "urdu"
+    //}
+    //for(let key in student){
+     //   console.log("key=",key,"val=",student[key])
+    //}
+
+
+
+   //  for(let num=0;num<=50;num++){
+   //  if( num % 2 !== 0);
+   //  console.log("num=",num);
+   //  }
+
+
+
+   let str = "helloWorld";
+   console.log(str);
+
+   
+
+    
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      
+    
